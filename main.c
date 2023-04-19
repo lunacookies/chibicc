1633a31539722af9b647681765170357bdd72800
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-04-19 13:23:41 +1000

    Add + and - operators

--- main.c
+++ main.c
@@ -8,9 +8,29 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	char *p = argv[1];
+
 	printf(".intel_syntax noprefix\n");
 	printf(".global _main\n");
 	printf("_main:\n");
-	printf("\tmov\trax, %d\n", atoi(argv[1]));
+	printf("\tmov\trax, %ld\n", strtol(p, &p, 10));
+
+	while (*p) {
+		if (*p == '+') {
+			p++;
+			printf("\tadd\trax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		if (*p == '-') {
+			p++;
+			printf("\tsub\trax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "unexpected character: '%c'\n", *p);
+		return 1;
+	}
+
 	printf("\tret\n");
 }
